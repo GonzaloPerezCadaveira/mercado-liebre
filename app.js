ffa414ee2257e411898af6d4ec8386228e3cb0c7
GonzaloPerezCadaveira <gperezmdp@gmail.com>
2022-07-04 21:10:36 -0300

    port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require ('path')
 
 app.use(express.static('Public'));
 
-app.listen (3000, () => { 
+app.listen (process.env.PORT || 3000, () => { 
 console.log ('servidor corriendo')
 })
 
