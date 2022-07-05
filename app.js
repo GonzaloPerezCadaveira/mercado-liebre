d36339e686d66d30b529ae55996a1eb39006f3eb
GonzaloPerezCadaveira <gperezmdp@gmail.com>
2022-07-04 22:12:20 -0300

    Procfile final

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require ('express')
 const app = express ()
 const path = require ('path')
+const port =process.env.PORT || 3000
 
 app.use(express.static('Public'));
 
-app.listen (process.env.PORT || 3000, () => { 
-console.log ('servidor corriendo')
-})
+app.listen(port, () => console.log(`Server running in port ${port}`));
+//app.listen (process.env.PORT || 3000, () => { 
+//console.log ('servidor corriendo')}
+
 
 app.get('/', (req, res) => {
     res.sendFile (path.resolve(__dirname,'./views/home.html'))
