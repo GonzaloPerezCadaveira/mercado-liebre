235371d2c775ebed7c16bf5f5f2c8b1b7c55b58c
GonzaloPerezCadaveira <gperezmdp@gmail.com>
2022-07-04 22:25:51 -0300

    Prueba final

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express ()
 const path = require ('path')
 const port =process.env.PORT || 3000
 
-app.use(express.static('Public'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Server running in port ${port}`));
 //app.listen (process.env.PORT || 3000, () => { 
